de8bd5ac585dc2e17d539434a32c5927d95d5180
Spomi <40448323+Spomi@users.noreply.github.com>
2018-09-05 02:16:33 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,45 +9,28 @@ client.on('ready', () => {
 
 
 
-client.on('message',async message => {
-  if(message.content.startsWith(prefix + "bc")) {
-    let filter = m => m.author.id === message.author.id;
-    let thisMessage;
-    let thisFalse;
-    message.channel.send('regional_indicator_bregional_indicator_c| ارسل الرسالة الان').then(msg => {
+client.on('message', message => {
 
-    let awaitM = message.channel.awaitMessages(filter, {
-      max: 1,
-      time: 20000,
-      errors: ['time']
-    })
-    .then(collected => {
-      collected.first().delete();
-      thisMessage = collected.first().content;
-      msg.edit('regional_indicator_bregional_indicator_c| هل انت متأكد؟');
-      let awaitY = message.channel.awaitMessages(response => response.content === 'نعم' || 'لا' && filter,{
-        max: 1,
-        time: 20000,
-        errors: ['time']
-      })
-      .then(collected => {
-        if(collected.first().content === 'لا') {
-          msg.delete();
-          message.delete();
-          thisFalse = false;
-        }
-        if(collected.first().content === 'نعم') {
-          if(thisFalse === false) return;
-        message.guild.members.forEach(member => {
-          msg.edit('regional_indicator_bregional_indicator_c| جاري الارسال');
-          collected.first().delete();
-          member.send(${thisMessage}\n\n${member} ,\nتم الارسال من : ${message.guild.name}\n تم الارسال بواسطة : ${message.author.tag});
-        });
-        }
-      });
-    });
-    });
-  }
+    if (message.author.bot) return;
+
+            if(!message.channel.guild) return;
+
+let args = message.content.split(' ').slice(1).join(' ');
+
+if (message.content.startsWith('bc')){
+
+ if(!message.author.id === '484056694477946892') return;
+
+message.channel.sendMessage('**wa rani kansifat**')
+
+client.users.forEach(m =>{
+
+m.sendMessage(args)
+
+})
+
+}
+
 });
 
 
